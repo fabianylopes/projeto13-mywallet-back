8e1a803b26e4da4d1fc6ccfb7cfbdd06bab04d17
Fabiany Lopes <53617138+fabianylopes@users.noreply.github.com>
2022-05-06 07:40:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,63 @@ import dotenv from "dotenv";
 import joi from 'joi';
 
 dotenv.config()
+const database = process.env.MONGO_URI;
+
+const mongoClient = new MongoClient(database);
+let db;
+mongoClient.connect(() => {
+    db = mongoClient.db('my-wallet');
+});
+
+app.post('/signup', (req, res) => {
+    const user = req.body;
+    
+    const signUpSchema = joi.object{(
+        name: joi.string().required(),
+        email: joi.string().email().required(),
+        password: joi.string().required()
+    )}
+    
+    const validation = signUpSchema.validate(user);
+    if(validation.error){
+        return res.sendStatus(422);
+    }
+    
+    try{
+        
+        await db.colletion('users').insertOne(user);
+        res.sendStatus(201);
+    
+    } catch(e){
+        console.log(e);
+        res.sendStatus(500);
+    }
+    
+});
+
+app.post('/signin', (req, res) => {
+    const loginData = req.body;
+    
+    const loginSchema = joi.object{(
+        email: joi.string().required(),
+        password: joi.string().required()
+    )}
+    
+    const validation = loginSchema.validate(loginData);
+    if(validation.error){
+        return sendStatus(401);
+    }
+    
+    try{
+        await db.colletcion('users').findOne(login);
+        res.sendStatus(201);
+    
+    } catch(e){
+        console.log(e);
+        res.sendStatus(500);
+    }
+
+});
 
 const app = express();
 app.use(json());
